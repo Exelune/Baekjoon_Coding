ccea823482b7c3266024152a59c5dd2f3ccc3108
Exelune <exelune703@gmail.com>
2025-04-21 20:37:04 +0900

    [Bronze V] Title: A-B, Time: 52 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var list = Console.ReadLine().Split(' ');
+        int a = int.Parse(list[0]);
+        int b = int.Parse(list[1]);
+        Console.WriteLine(a - b);
+    }
+}
