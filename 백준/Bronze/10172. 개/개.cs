e15b153f28e4470a3b05030dd5b2eabee246e803
Exelune <exelune703@gmail.com>
2025-04-21 21:12:21 +0900

    [Bronze V] Title: 개, Time: 44 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("|\\_/|");
+        Console.WriteLine("|q p|   /}");
+        Console.WriteLine("( 0 )\"\"\"\\");
+        Console.WriteLine("|\"^\"`    |");
+        Console.WriteLine("||_/=\\\\__|");
+    }
+}
