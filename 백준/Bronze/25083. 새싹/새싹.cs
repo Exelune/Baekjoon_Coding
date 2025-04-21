30d5418cf48361cb8c785d592c1e03b7afaffb8d
Exelune <exelune703@gmail.com>
2025-04-22 03:38:42 +0900

    [Bronze V] Title: 새싹, Time: 48 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("         ,r'\"7");
+        Console.WriteLine("r`-_   ,'  ,/");
+        Console.WriteLine(" \\. \". L_r'");
+        Console.WriteLine("   `~\\/");
+        Console.WriteLine("      |");
+        Console.WriteLine("      |");
+    }
+}
