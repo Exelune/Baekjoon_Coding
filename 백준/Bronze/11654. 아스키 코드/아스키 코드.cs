ea984df0607bc575e2347a7f64db854ab9d07295
Exelune <exelune703@gmail.com>
2025-04-22 10:25:05 +0900

    [Bronze V] Title: 아스키 코드, Time: 52 ms, Memory: 5448 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        char x = input[0];
+        int ascll = (int)x;
+        Console.WriteLine(ascll);
+    }
+}
