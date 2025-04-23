fe37d2b6a2836bbd1a62fc1fba804dc4942319f6
Exelune <exelune703@gmail.com>
2025-04-23 16:41:41 +0900

    [Bronze V] Title: A+B - 5, Time: 52 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string line;
+        while ((line = Console.ReadLine()) != null)
+        {
+            var list = line.Split();
+            int a = int.Parse(list[0]);
+            int b = int.Parse(list[1]);
+            if(a == 0 && b == 0)
+            {
+                break;
+            }
+            Console.WriteLine(a + b);
+        }
+    }
+}
