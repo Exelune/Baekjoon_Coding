3b40c2e67cfb12c63aaf609e922ed07cc8d4b6e0
Exelune <exelune703@gmail.com>
2025-05-05 00:46:59 +0900

    [Silver V] Title: 최소공배수, Time: 52 ms, Memory: 5732 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var input = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+        var (a, b) = (input[0], input[1]);
+
+        Console.WriteLine(LCM(a, b));
+    }
+
+    static long GCD(long a, long b)
+    {
+        long r = 0;
+
+        while(b != 0)
+        {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+
+        return a;
+    }
+
+    static long LCM(long a, long b)
+    {
+        return a * b / GCD(a, b);
+    }
+}
