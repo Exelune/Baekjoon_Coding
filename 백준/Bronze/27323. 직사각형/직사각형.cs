f1f42f01d90dc911d0bdbc470bc212023e714d8a
Exelune <exelune703@gmail.com>
2025-05-07 20:17:04 +0900

    [Bronze V] Title: 직사각형, Time: 48 ms, Memory: 5448 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(a * b);
+    }
+}
