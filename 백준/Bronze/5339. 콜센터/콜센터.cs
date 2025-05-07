f29e5732760e1eb27bea7d89b4969697d4de4c14
Exelune <exelune703@gmail.com>
2025-05-07 20:19:32 +0900

    [Bronze V] Title: 콜센터, Time: 44 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("     /~\\");
+        Console.WriteLine("    ( oo|");
+        Console.WriteLine("    _\\=/_");
+        Console.WriteLine("   /  _  \\");
+        Console.WriteLine("  //|/.\\|\\\\");
+        Console.WriteLine(" ||  \\ /  ||");
+        Console.WriteLine("============");
+        Console.WriteLine("|          |");
+        Console.WriteLine("|          |");
+        Console.WriteLine("|          |");
+    }
+}
