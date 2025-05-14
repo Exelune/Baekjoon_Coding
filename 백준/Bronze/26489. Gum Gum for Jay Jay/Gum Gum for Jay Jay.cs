ac18228f785c7fcb9dc11ee8a629847eaaa4b7d9
Exelune <exelune703@gmail.com>
2025-05-14 22:08:17 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int i = 0;
+        string s;
+
+        while ((s = sr.ReadLine()) != null)
+        {
+            i++;
+        }
+
+        sw.WriteLine(i);
+    }
+}
