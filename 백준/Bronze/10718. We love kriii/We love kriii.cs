271f272e5c29d369b2be63b42bd4c1659c990550
Exelune <exelune703@gmail.com>
2025-05-26 00:10:12 +0900

    [Bronze V] Title: We love kriii, Time: 44 ms, Memory: 5336 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        sw.WriteLine("강한친구 대한육군");
+        sw.WriteLine("강한친구 대한육군");
+    }
+}
