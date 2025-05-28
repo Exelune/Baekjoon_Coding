5ec3994ff42389c19d1457a9d2839bc8197edf9d
Exelune <exelune703@gmail.com>
2025-05-28 22:38:30 +0900

    [Bronze V] Title: 홍익대학교, Time: 52 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int input = int.Parse(sr.ReadLine());
+
+        sw.WriteLine(input - 1946);
+    }
+}
