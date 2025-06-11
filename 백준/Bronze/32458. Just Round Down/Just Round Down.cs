5b5807f469e74c111da3c05bc580d11a516c6eda
Exelune <exelune703@gmail.com>
2025-06-11 13:29:59 +0900

    [Bronze V] Title: Just Round Down, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        double A = double.Parse(sr.ReadLine());
+
+        sw.Write((int)A);
+        
+    }
+}
