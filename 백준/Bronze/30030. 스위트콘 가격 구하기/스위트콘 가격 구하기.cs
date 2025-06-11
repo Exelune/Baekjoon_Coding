dec7afa1742a6bd28cb17f1e00cb24f36ad70632
Exelune <exelune703@gmail.com>
2025-06-11 13:25:19 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 48 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int A = int.Parse(sr.ReadLine());
+        int A2 = A / 11;
+
+        sw.WriteLine(A - A2);
+        
+    }
+}
