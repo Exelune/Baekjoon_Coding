45fae0e058e9444cdfe3cc4063a2b4d65e967107
Exelune <exelune703@gmail.com>
2025-06-15 21:50:54 +0900

    [Bronze V] Title: 긴자리 계산, Time: 52 ms, Memory: 5748 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        BigInteger A = BigInteger.Parse(sr.ReadLine());
+        BigInteger B = BigInteger.Parse(sr.ReadLine());
+
+        sw.WriteLine(A + B);
+        sw.WriteLine(A - B);
+        sw.WriteLine(A * B);
+
+    }
+}
