e9fe04354e863c6a42aa90c8329841fb711b9ec1
Exelune <exelune703@gmail.com>
2025-06-18 06:46:15 +0900

    [Bronze V] Title: Java Warriors, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int a = int.Parse(sr.ReadLine());
+        sw.WriteLine(a * 4000);
+    }
+}
