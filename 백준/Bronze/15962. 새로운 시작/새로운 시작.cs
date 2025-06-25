f3659447c293691f8124cbdc6cc884b8ac5bcb4c
Exelune <exelune703@gmail.com>
2025-06-25 22:06:37 +0900

    [Bronze V] Title: 새로운 시작, Time: 44 ms, Memory: 5348 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        sw.WriteLine("파이팅!!");
+    }
+}
