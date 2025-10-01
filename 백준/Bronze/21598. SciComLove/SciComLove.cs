98d704d8f85769573d2a1235c9c104b163c459fb
Exelune <exelune703@gmail.com>
2025-10-01 10:03:21 +0900

    [Bronze V] Title: SciComLove, Time: 48 ms, Memory: 5604 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int N = int.Parse(sr.ReadLine());
+
+        for (int i = 1; i <= N; i++)
+        {
+            sw.WriteLine("SciComLove");
+        }
+    }
+}
