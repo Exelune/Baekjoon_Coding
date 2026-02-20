0baea0f899f616faf6a868fb022ff581d9b1bd5d
Exelune <exelune703@gmail.com>
2026-02-20 21:15:50 +0900

    [Bronze V] Title: Micromasters, Time: 52 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Bronze/33178. Micromasters/Micromasters.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        sw.WriteLine(int.Parse(sr.ReadLine()) / 10);
+    }
+}
