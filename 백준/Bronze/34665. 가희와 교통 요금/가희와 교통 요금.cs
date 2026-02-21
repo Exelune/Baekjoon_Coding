7c44536ca6a38bc61b3b986e3b703b5c1fb039f1
Exelune <exelune703@gmail.com>
2026-02-21 13:35:13 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 48 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        string a = sr.ReadLine();
+        string b = sr.ReadLine();
+
+        if (a != b) sw.WriteLine(1550);
+        else sw.WriteLine(0);
+    }
+}
